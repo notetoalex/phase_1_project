4ac28877e56f80e76f550ffd52b8f317d7ffd46e
Alex Lee <lee.a.esq@gmail.com>
2021-09-20 02:39:38 -0700

    Added login logic

--- src/b_login/B_SearchUsernames.java
+++ src/b_login/B_SearchUsernames.java
@@ -1,7 +1,10 @@
 package b_login;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 public class B_SearchUsernames {
 
@@ -19,30 +22,98 @@ public class B_SearchUsernames {
 		accounts.put("minnie", "mouse");
 		
 		
-		String username = "tweety";
+		String searchKey = "silvester";		//	this is a placeholder to capture the inputed username
+		String proposedValue = "cat";		//	this is a placeholder to capture inputed password
 		
-		if  (accounts.get(username) != null) {
-			
-			System.out.println(username+" password is: "+accounts.get(username));
-		} 
-		else {
-			System.out.println("The username is not found.");
-		} 
-			
-		/*	iterate map - did not preserve order
-		for(Map.Entry<String, String> entry : accounts.entrySet()) {
-			System.out.println("Key : "+entry.getKey());
-			System.out.println("Value : "+entry.getValue());
-			System.out.println("------");
-			
-			
+		int attemptUsername = 0;			//	initializing counters for the attempts
+		int attemptPassword = 0;
 		
+		boolean statusUsername = false;		//	initializing status for user
+		boolean statusPassword = false;		//	initializing status for password
+		
+		boolean statusLogin = false;		//	initializing flag that indicates whether the user has successfully logged in.
+		
+
+		//	username section for login
+		while ((statusUsername==false) && (attemptUsername < 3)){
+		
+			//	username successfully entered
+			if (accounts.containsKey(searchKey)==true) {
+				statusUsername = true;
+				System.out.println("username found.");
+				
+			//	username incorrectly entered or not found (3x)
+			} else if (attemptUsername < 3){
+
+		        System.out.println("The username was not found. You have "+(2-attemptUsername)+" more attempt(s) remaining before the program exits." );
+		        System.out.println("Please try again.");
+				attemptUsername ++;
+
+			}
+			
 		}
-		*/
+
+			//	shutting down the application after 3 attempts
+			if (attemptUsername >= 3){
+		    	System.out.println("You have exceeded the number of allowed attempts for username.  Goodbye.");
+		    	System.exit(0);	// terminates JVM
+		}
+
+
+		//	password section for login
+		while ((statusPassword==false) && (attemptPassword < 3)){
+			
+			String searchValue = getValues(accounts, searchKey);
+			
+			//	password successfully entered
+			if (searchValue.equals("["+proposedValue+"]")) {	// had to include the brackets because for the method returns a set that can't be compared to a string directly
+				statusPassword = true;
+				System.out.println("password is valid.");
+			
+			//	incorrect password (3 attempts allowed)
+			} else if (attemptPassword < 3){
+
+		        System.out.println("The password is not valid. You have "+(2-attemptPassword)+" more attempt(s) remaining before the program exits." );
+		        System.out.println("Please try again.");
+				attemptPassword ++;
+
+			} 
+			
+		}
 		
-		
-		
-		
-	}
+			//	shutting down the application after 3 attempts
+			if (attemptPassword >= 3){
+		    	System.out.println("You have exceeded the number of allowed attempts for the password.  Goodbye.");
+		    	System.exit(0);	// terminates JVM
+			}
+
+
+		//	correct username and password
+		if ((statusUsername = true) && (statusPassword = true)) {
+			statusLogin = true;
+			System.out.println("Thank you.  You are now successfully logged in.");
+		}
+
+		}
+					
+
+//	defining the method to obtain the password (value) associated with a username (key) 
+private static String getValues(Map<String, String> map, String key) {
+
+      Set<String> result = new HashSet<>();
+      if (map.containsKey(key)) {
+          for (Map.Entry<String, String> entry : map.entrySet()) {
+              if (Objects.equals(entry.getKey(), key)) {
+                  result.add(entry.getValue());
+              }
+
+	      }
+	     
+	      
+      }
+	     
+      return result.toString();	//	The original result is in the form of a Set, and contains "[]" around the string.
+
+}
 
 }
